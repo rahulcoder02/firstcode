1728fd946c3788ad0ea93488619f67c4b057a804
rahulcoder02 <rajrahulchoudhary03@gmail.com>
2025-07-16 01:10:57 +0530

    first cpp code

---
+++ cpp.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+
+    int a = 12;
+
+    int b=12;
+
+     int c =a +b; 
+
+     cout << " print sum :" << c <<endl; // output = 24;
+     
+    return 0;
+}
