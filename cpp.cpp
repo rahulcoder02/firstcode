417314ea989807f622854c830c5b15924dd47758
rahulcoder02 <rajrahulchoudhary03@gmail.com>
2025-07-16 01:35:08 +0530

    m

--- cpp.cpp
+++ cpp.cpp
@@ -1,15 +1,32 @@
 #include<iostream> 
+
 using namespace std;
 
+
+
 int main(){
+int a;
+int b;
 
-    int a = 12;
+cout << " enter your number sum :"  <<endl;
+cin>> a ;
+cin>>b;
 
-    int b=12;
 
-     int c =a +b; 
 
-     cout << " print sum :" << c <<endl; // output = 24;
-     
+     cout << " print sum :" << a+b <<endl; // output = 15;
+
     return 0;
 }
+
+
+// int main(){
+//     int a=10;
+//     int b=5;
+//     int c=a+b;
+
+//     cout <<"printf sum:"<<c<<endl;
+//     return 0;
+
+// }
+
